0368ed84b972e098ac44ed879ee4a3f39b66a0f6
felixm-byte <72092800+felixm-byte@users.noreply.github.com>
2024-07-25 20:34:56 +0100

    Developed basic dialogue system
    and wrote dialogue of the first meeting, which is with the department leads.

--- scenes/main.gd
+++ scenes/main.gd
@@ -7,7 +7,10 @@ extends Control
 var state: GameState
 var is_in_pause_menu: bool = false
 #endregion
-
+#region Constants
+@export var interactions : Array[DialogueInteraction]
+const EVENT_SELECTION_PROPERTIES: Array[String] = ["turn"]
+#endregion
 func _ready():
 	pause_menu.resume.connect(func(): is_in_pause_menu = false)
 
@@ -17,3 +20,20 @@ func _input(event):
 	
 	if event.is_action_pressed("escape"):
 		pause_menu.open_pause_menu()
+
+func start_next_dialogue() -> void:
+	var next_event: DialogueInteraction
+	for event in interactions:
+		var selection_valid := true
+		
+		for property in EVENT_SELECTION_PROPERTIES:
+			if (event[property] != null) and !(event[property] == state[property]):
+				selection_valid = false
+		
+		if selection_valid:
+			next_event = event
+			break
+	
+	var dialogue = load(next_event.path_to_dialogue)
+	if dialogue == OK:
+		DialogueManager.show_dialogue_balloon(dialogue)
